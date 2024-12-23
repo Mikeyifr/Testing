f948cfd540e39dda025ef3d1128a5e841fdfe79d
Mikeyifr <michelyfrah@gmail.com>
2024-12-23 21:30:39 -0200

    working for now

--- ci.sh
+++ ci.sh
@@ -2,7 +2,6 @@
 
 # Pull the latest changes from the 'stage' branch and redirect output to log.txt
 git pull origin stage >> /home/mikey/testci/log.txt 2>&1
-git checkout stage >> /home/mikey/testci/log.txt
 
 # Get the version from package.json and append it to log.txt
 version=$(jq -r '.version' package.json)
@@ -12,10 +11,10 @@ echo "Version: $version" >> /home/mikey/testci/log.txt
 commit_sha=$(git log -n 1 --pretty=format:"%H" develop | cut -c1-7)
 echo "Commit SHA: $commit_sha" >> /home/mikey/testci/log.txt
 
-build="$version-$commit_sha"
+export build="$version-$commit_sha"
 
 # Change the image of the docker-compose service to the new one
-yq e '.services.app.image = "$build"' -i /home/mikey/testci/docker-compose.yml >> /home/mikey/testci/log.txt 2>&1
+yq e '.services.app.image = strenv(build)' -i /home/mikey/testci/docker-compose.yml >> /home/mikey/testci/log.txt 2>&1
 
 # Build the Docker image and append the output to log.txt
 docker build -t "$build" /home/mikey/testci/ >> /home/mikey/testci/log.txt 2>&1
